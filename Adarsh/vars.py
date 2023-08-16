f821a988f87184aff79c8677367c8ff5b084577b
qtvsprofff <142015253+qtvsprofff@users.noreply.github.com>
2023-08-16 15:13:31 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,18 +9,18 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '11427887'))
-    API_HASH = str(getenv('API_HASH', '317ea7bfc3dcbfb2a4aa7c866110074e'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6045448887:AAEk9V2UmzBKjS0jIOl5Du971gc5SYAB5mA'))
+    API_ID = int(getenv('API_ID', '13415474'))
+    API_HASH = str(getenv('API_HASH', '01bb828c0429beeabd6e9e841d026231'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6434399232:AAFy94DNlPbOjzQlrMEoKcwjeHWRnJGmxFI'))
     SESSION_NAME = str(getenv('SESSION_NAME', 'qtmvelinkz'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001859639118'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001848305113'))
     PORT = int(getenv('PORT', 8080))
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1044163529").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "885675538").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     SHORTENER_API = str(getenv('SHORTENER_API', '02c93c55c4567035a37ffc32731d8f0e1c530f98'))
     SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnshort.net'))
