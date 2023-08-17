19920afed8c6a42984957546e0c551c08ad94558
qtvsprofff <142015253+qtvsprofff@users.noreply.github.com>
2023-08-17 13:56:15 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     API_ID = int(getenv('API_ID', '13415474'))
     API_HASH = str(getenv('API_HASH', '01bb828c0429beeabd6e9e841d026231'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6434399232:AAFy94DNlPbOjzQlrMEoKcwjeHWRnJGmxFI'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'qtmvelinkz'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'SAM_DUB_LEZHa'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001848305113'))
@@ -22,15 +22,15 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "885675538").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    SHORTENER_API = str(getenv('SHORTENER_API', '02c93c55c4567035a37ffc32731d8f0e1c530f98'))
-    SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnshort.net'))
+    SHORTENER_API = str(getenv('SHORTENER_API', 'ab902e9f48d0f651e1bca4fee694b0efc85a1cf2'))
+    SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'omnifly.in.net'))
     FDQN = str(getenv('FDQN', 'https://qtmvelinkz.onrender.com'))
 
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('Owner_of_qtmve'))
+    OWNER_USERNAME = str(getenv('SMD_Owner'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', 'qtmvelinkz'))
+        APP_NAME = str(getenv('APP_NAME', 'SAM_DUB_LEZHa'))
     
     else:
         ON_HEROKU = False
@@ -41,5 +41,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://hillsking1222:kuraman1@kumaran1.d6ahc05.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@qtmve_linkzZz"))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@SAM_DUB_LEZHa"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
