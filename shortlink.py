f13b6a93b281aac3ea24a724b828ad031400b21a
qtvsprofff <142015253+qtvsprofff@users.noreply.github.com>
2023-08-17 13:53:49 +0530

    Update shortlink.py

--- shortlink.py
+++ shortlink.py
@@ -5,8 +5,8 @@ from asyncio import TimeoutError
 from shortzy import Shortzy
 from Adarsh.vars import Var
 
-SHORTENER_API = str(getenv('SHORTENER_API', 'e0867ce24e2238645541bf7651be2217b4cd5dd1'))
-SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'shorturllink.in'))
+SHORTENER_API = str(getenv('SHORTENER_API', 'ab902e9f48d0f651e1bca4fee694b0efc85a1cf2'))
+SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'omnifly.in.net'))
 
 
 shortzy = Shortzy(SHORTENER_API, SHORTENER_WEBSITE)
